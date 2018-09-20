1ee92384ef3232bc631c1b593e4a9502563199f4
IagoLast <iago.lastra@gmail.com>
2018-09-20 20:28:28 +0200

    Add infinite scroll

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,28 +5,51 @@ import './App.css';
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = { pictures: [] };
+    this.state = { pictures: [], fetching: false };
   }
 
   componentWillMount() {
+    this.setState({ fetching: true });
     fetch('http://localhost:3000/api/v1/pics')
       .then(raw => raw.json())
       .then(pictures => {
-        this.setState({ pictures });
-      })
+        this.setState({ pictures, fetching: false });
+      });
+  }
+
+  componentDidMount() {
+    window.addEventListener('scroll', () => {
+      if (window.pageYOffset > 0.6 * document.body.scrollHeight) {
+        this.fetchPictures();
+      }
+    });
+  }
+
+  fetchPictures() {
+    if (this.state.fetching) {
+      return;
+    }
+    this.setState({ fetching: true });
+    return fetch('http://localhost:3000/api/v1/pics')
+      .then(raw => raw.json())
+      .then(pictures => {
+        const newArray = this.state.pictures.concat(pictures);
+        this.setState({ pictures: newArray, fetching: false });
+      });
   }
 
   render() {
     return (
       <section className="gallery">
         {this.state.pictures.map(this.renderPicture)}
+        {this.state.fetching ? 'Loading...' : ''}
       </section>
     );
   }
 
 
-  renderPicture(data) {
-    return <figure class="pic">
+  renderPicture(data, index) {
+    return <figure key={index} className="pic">
       <img className="pic__img" src={data.url_s} alt={data.title} title={data.title} />
       <figcaption className="pic__caption">{data.title} by {data.ownername}</figcaption>
     </figure>
