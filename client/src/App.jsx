fe08be18dca0f065865de9af5de4dcf1541e8928
IagoLast <iago.lastra@gmail.com>
2018-09-20 21:28:49 +0200

    Show more data on focus

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -49,9 +49,23 @@ class App extends Component {
 
   renderPicture(data, index) {
     return <figure onClick={() => this.onImageClick(index)} key={index} className={this.state.active === index ? 'pic active' : 'pic'} >
-      <img className="pic__img" src={data.url_s} alt={data.title} title={data.title} />
-      <figcaption className="pic__caption">{data.title} by {data.ownername}</figcaption>
+      <img className="pic__img" src={this.state.active === index ? data.url_l : data.url_s} alt={data.title} title={data.title} />
+      <figcaption className="pic__caption">
+        {this.renderCaption(index, data)}
+      </figcaption>
     </figure>
+  }
+
+  renderCaption(index, data) {
+    if (this.state.active !== index) {
+      return `${data.title} by ${data.ownername}`;
+    }
+    return <React.Fragment>
+      <h1> {data.title} </h1>
+      <h3>{data.ownername}</h3>
+      <p>Date: {data.datetaken}</p>
+      <pre>{data.tags}</pre>
+    </React.Fragment>
   }
 
   onImageClick(index) {
