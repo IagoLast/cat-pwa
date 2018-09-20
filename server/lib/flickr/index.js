e30b5b42aaa41cfe4a10f6a0a508659568ed8f22
IagoLast <iago.lastra@gmail.com>
2018-09-20 19:10:39 +0200

    Get more fields from the API

--- server/lib/flickr/index.js
+++ server/lib/flickr/index.js
@@ -3,9 +3,12 @@ const {
   api_key
 } = require('../../config/dev.json');
 
+// https://www.flickr.com/services/api/flickr.photos.search.html#yui_3_11_0_1_1537461393936_336
+const extras = 'description, license, date_upload, date_taken, owner_name, icon_server, original_format, last_update, geo, tags, machine_tags, o_dims, views, media, path_alias, url_m, url_l';
+
 
 async function getPictures(tags = 'cats') {
-  const rawResponse = await fetch(`https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${api_key}&tags=${tags}&format=json&nojsoncallback=1`);
+  const rawResponse = await fetch(`https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${api_key}&tags=${tags}&extras=${extras}&format=json&nojsoncallback=1`);
   const response = await rawResponse.json();
   return response.photos.photo;
 }
--- server/test/flickr.spec.js
+++ server/test/flickr.spec.js
@@ -30,5 +30,21 @@ describe('flickr', () => {
       expect(pictures[0].title).to.exist;
     });
 
+    it('a picture should have a farm ', async () => {
+      expect(pictures[0].title).to.exist;
+    });
+
+    it('a picture should have a url_m ', async () => {
+      expect(pictures[0].url_m).to.exist;
+    });
+
+    it('a picture should have a url_l ', async () => {
+      expect(pictures[0].url_l).to.exist;
+    });
+
+    it('a picture should have a ownername ', async () => {
+      expect(pictures[0].ownername).to.exist;
+    });
+
   });
 });
