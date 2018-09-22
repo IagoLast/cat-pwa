101a48f92feba40aaee6b0429ced8ad0e63bec2f
IagoLast <iago.lastra@gmail.com>
2018-09-22 20:26:38 +0200

    Add in memory cache to get better response times

--- server/index.js
+++ server/index.js
@@ -1,16 +1,25 @@
 const express = require('express');
 const cors = require('cors');
-
+const NodeCache = require('node-cache');
 const flickr = require('./lib/flickr/index');
+
 
 const app = express();
 app.use(cors());
 app.use(express.static(__dirname + '/public'));
 
+const cache = new NodeCache({
+  stdTTL: 30
+});
+
 
 app.get('/api/v1/pics/:page', async (req, res) => {
   try {
+    if (cache.get(req.url)) {
+      return res.json(cache.get(req.url));
+    }
     const pictures = await flickr.getPictures('cats, kitten', req.params.page);
+    cache.set(req.url, pictures);
     res.json(pictures);
   } catch (e) {
     res.status(500);
